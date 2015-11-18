e1e295179947e2f07764d30da1ce2dde8a7513e7
Preston Richey <jpyrichey@gmail.com>
2015-11-18 14:42:58 -0700

    Create ModDictionaries.py

---
+++ ModDictionaries.py
@@ -0,0 +1,3 @@
+absorptionMods = {'Increased Maximum': 'adv.25', "Varying Effect": 'adv.75', 'Limited Phenomenon (Rare)': 'lim1', 'Limited Phenomenon (Uncommon)': 'lim.75', 'Limited Phenomenon (Common)': 'lim.5', 'Limited Phenomenon (Very Common)': 'lim.25'}
+aidMods = {'Only Others': 'lim.5', 'Only Self': 'lim1'}
+barrierMods = {'+1m length/height or +1/2 thickness': 'num1', '+1 Body'}
