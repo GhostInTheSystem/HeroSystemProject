8f206232dd5560bce5a37076b525e9e4aea15d2e
Preston Richey <jpyrichey@gmail.com>
2016-03-08 14:10:03 -0700

    Help

--- TkinterTest2.py
+++ TkinterTest2.py
@@ -13,26 +13,26 @@ w.grid(row=0, column=56, rowspan=952, columnspan=5)
 w.create_line(5, 0, 5, 952, fill="#000000", width=5)
 
 photo=PhotoImage(file="Test.gif")
-charDetailsButton = Button(master, image=photo, width=160, height = 136, command=lambda: pageSwitch("Character Details"))
+charDetailsButton = Button(master, image=photo, width=160, height = 127, command=lambda: pageSwitch("Character Details"))
 charDetailsButton.grid(row=0, column=1, rowspan=136, columnspan=55, padx=(25,10))
 
 #characteristicButton = Button(master, text="Characteristics", width=20, height=0, command=lambda: pageSwitch("Characteristics"))
-characteristicButton = Button(master, image=photo, width=160, height=136, command=lambda: pageSwitch("Characteristics"))
+characteristicButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Characteristics"))
 characteristicButton.grid(row=137, column=1, rowspan=136, columnspan=55, padx=(25,10))
 
-skillsButton = Button(master, text="Skills", width=20, height=0, command=lambda: pageSwitch("Skills"))
+skillsButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Skills"))
 skillsButton.grid(row=273, column=1, columnspan=55, rowspan=136, padx=(25,10))
 
-perkTalentButton = Button(master, text="Perks/Talents", width=20, height=0, command=lambda: pageSwitch("Perks/Talents"))
+perkTalentButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Perks/Talents"))
 perkTalentButton.grid(row=409, column=1, columnspan=55, rowspan=136, padx=(25,10))
 
-powerButton = Button(master, text="Powers", width=20, height=0, command=lambda: pageSwitch("Powers"))
+powerButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Powers"))
 powerButton.grid(row=545, column=1, columnspan=55, rowspan=136, padx=(25,10))
 
-complicationButton = Button(master, text="Complications", width=20, height=0, command=lambda: pageSwitch("Complications"))
+complicationButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Complications"))
 complicationButton.grid(row=681, column=1, columnspan=55, rowspan=136, padx=(25,10))
 
-charSheetButton = Button(master, text="Character Sheet", width=20, height=0, command=lambda: pageSwitch("Character Sheet"))
+charSheetButton = Button(master, image=photo, width=160, height=127, command=lambda: pageSwitch("Character Sheet"))
 charSheetButton.grid(row=817, column=1, columnspan=55, rowspan=136, padx=(25,10))
 
 testText = Text(master, height=1, width = 20)
